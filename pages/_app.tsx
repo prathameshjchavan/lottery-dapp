c91fac29215a3430df6216182c4ce0dc1ab498a3
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-04-22 20:11:41 +0530

    change network to sepolia

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { Sepolia } from "@thirdweb-dev/chains";
 import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<ThirdwebProvider
-			activeChain="mumbai"
+			activeChain={Sepolia}
 			clientId={process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID}
 		>
 			<Component {...pageProps} />
